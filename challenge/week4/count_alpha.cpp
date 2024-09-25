2f3fd47f374aaa2382e6527ec0e0c51ca6b17294
조영진(Joyoungjin) <slugger613@naver.com>
2024-09-25 16:20:41 +0900

    Loop Exercises

--- challenge/week4/count_alpha.cpp
+++ challenge/week4/count_alpha.cpp
@@ -13,28 +13,32 @@ int main() {
         cin >> ch;
         if (cin.eof())  // Ctrl+Z = EOF를 터미널에서 처리하기 위한 cin.eof() 사용
             break;
-        
-        switch (ch)     // 모음인 경우 vowel++, 그 외의 경우 consonant++
-        {
-        case 'a':
-            vowel++;
-            break;
-        case 'e':
-            vowel++;
-            break;
-        case 'i':
-            vowel++;
-            break;
-        case 'o':
-            vowel++;
-            break;
-        case 'u':
-            vowel++;
-            break;
-        default:
-            consonant++;
-            break;
+        if (ch < 96 || ch > 122)
+            cout << "영어 소문자 이외의 문자가 입력되었습니다. 다시 입력해주세요." << endl;
+        else {
+            switch (ch)     // 모음인 경우 vowel++, 그 외의 경우 consonant++
+            {
+            case 'a':
+                vowel++;
+                break;
+            case 'e':
+                vowel++;
+                break;
+            case 'i':
+                vowel++;
+                break;
+            case 'o':
+                vowel++;
+                break;
+            case 'u':
+                vowel++;
+                break;
+            default:
+                consonant++;
+                break;
+            }
         }
+        
     }
     // 결과 출력
     cout << "모음: " << vowel << endl;
